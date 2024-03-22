533a435c35ce1db986445fdebc9eadcd4084241a
Billy <ismail.bilkisu@gmail.com>
2024-03-22 14:15:44 +0100

    email added

--- components/header.tsx
+++ components/header.tsx
@@ -5,11 +5,11 @@ import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
-// import { useActiveSectionContext } from "@/context/active-section-context";
+ import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Header() {
-//   const { activeSection, setActiveSection, setTimeOfLastClick } =
-//     useActiveSectionContext();
+ const { activeSection, setActiveSection, setTimeOfLastClick } =
+  useActiveSectionContext();
 
   return (
     <header className="z-[999] relative">
@@ -24,7 +24,7 @@ export default function Header() {
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
-      {/* <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
@@ -37,8 +37,7 @@ export default function Header() {
                 className={clsx(
                   "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
                   {
-                    "text-gray-950 dark:text-gray-200":
-                      activeSection === link.name,
+                    "text-gray-950 dark:text-gray-200": activeSection === link.name,
                   }
                 )}
                 href={link.hash}
@@ -64,7 +63,7 @@ export default function Header() {
             </motion.li>
           ))}
         </ul>
-      </nav> */}
+      </nav>
     </header>
   );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,17 +1,26 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import React from 'react'
-import { motion } from 'framer-motion'
-import Link from 'next/link'
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
-const Intro = () => {
+export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
-      <div className="flex items-center justify-center rounded-full">
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
@@ -23,14 +32,15 @@ const Intro = () => {
           >
             <Image
               src="/profile.jpg"
-              alt="Bilqisu portrait"
+              alt="Ricardo portrait"
               width="192"
               height="192"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
+
           <motion.span
             className="absolute bottom-0 right-0 text-4xl"
             initial={{ opacity: 0, scale: 0 }}
@@ -46,14 +56,15 @@ const Intro = () => {
           </motion.span>
         </div>
       </div>
+
       <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Bilqisu.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">2 years</span> of experience. I enjoy
+        <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
@@ -89,7 +100,7 @@ const Intro = () => {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/bilqisu-ismail-48192673/"
+          href="https://linkedin.com"
           target="_blank"
         >
           <BsLinkedin />
@@ -97,7 +108,7 @@ const Intro = () => {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/ismailbilly"
+          href="https://github.com"
           target="_blank"
         >
           <FaGithubSquare />
@@ -106,5 +117,3 @@ const Intro = () => {
     </section>
   );
 }
-
-export default Intro
